efc6856169522e988380073d4cd656a90203ba49
Davide <davide.mattioli.2001@gmail.com>
2025-04-24 18:15:20 +0200

    Add Excess Green filtering functionality and enhance logging in OSAVI index filtering

--- Main/02_filtering.py
+++ Main/02_filtering.py
@@ -1,36 +1,48 @@
-from functions.filters_functions import  OSAVI_index_filtering
+import logging
+
+from functions.filters_functions import OSAVI_index_filtering, excess_green_filter
 from functions.config_object import config_object
 import polars as pl
 import os
 import glob
+
+def logging_config():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler("process.log", encoding='utf-8'),  # Note the encoding parameter
+            logging.StreamHandler()
+        ]
+    )
+
+
+
 def main():
     config = config_object("config_file.yaml")
+    logging_config()
 
     ## Import dataframe
 
-    print(config.main_extract_out)
 
     paths = glob.glob(os.path.join(config.main_extract_out, "*.parquet"))
 
     df = pl.read_parquet(paths[3])
 
-    print(f"Dataframe shape before filtering: {df.shape}")
 
     ## Apply OSAVI index filtering
     df = OSAVI_index_filtering(df)
+    df = excess_green_filter(df)
 
-    print(f"Dataframe shape after filtering: {df.shape}")
+
+    print(df["OSAVI"].min())
+
+
 
 
     print(df)
 
 
     ##glob files
-
-
-
-
-    print(paths)
-
 if __name__ == "__main__":
     main()
--- Main/functions/filters_functions.py
+++ Main/functions/filters_functions.py
@@ -1,5 +1,4 @@
 import logging
-
 import polars as pl
 def OSAVI_index_filtering(df):
     """
@@ -15,17 +14,46 @@ def OSAVI_index_filtering(df):
         # Calculate OSAVI index
         Y = 0.16
         df = df.with_columns([
-            (1 + Y) * ((pl.col("band5") - pl.col("band3")) / (pl.col("band5") + pl.col("band3") + Y)).alias("OSAVI")
+            ((1 + Y) * ((pl.col("band5") - pl.col("band3")) / (pl.col("band5") + pl.col("band3") + Y))).alias("OSAVI")
         ])
 
         # Verify OSAVI column creation
-        if "OSAVI" not in df.columns:
-            raise ValueError("Failed to create 'OSAVI' column in the DataFrame.")
 
         # Filter based on OSAVI index
-        df = df.filter(pl.col("OSAVI") > 0.1)
+        len_before = len(df)
+        df = df.filter(pl.col("OSAVI") > 0.7)
+
+        logging.info(f"OSAVI filtering: {len_before} -> {len(df)} | Percentage of points filtered: {(len_before-len(df))/len_before * 100}%" )
+
 
         return df
     except Exception as e:
         logging.error(f"Error in OSAVI index filtering: {e}")
+        raise
+def excess_green_filter(df):
+    """
+    Apply the Excess Green index filtering to the DataFrame.
+    """
+    try:
+        # Ensure required columns exist
+        required_columns = ["band1", "band2", "band3"]
+        missing_columns = [col for col in required_columns if col not in df.columns]
+        if missing_columns:
+            raise ValueError(f"Missing required columns for Excess Green calculation: {missing_columns}")
+
+        # Calculate Excess Green index
+        df = df.with_columns([
+            (2*pl.col("band2")-pl.col("band3")-pl.col("band1")).alias("ExcessGreen")
+        ])
+
+        len_before = len(df)
+
+        # Filter based on Excess Green index
+        df = df.filter(pl.col("ExcessGreen") > 0.05)
+        logging.info(f"Excess Green filtering: {len_before} -> {len(df)} | Percentage of points filtered: {(len_before-len(df))/len_before * 100}%" )
+
+
+        return df
+    except Exception as e:
+        logging.error(f"Error in Excess Green filtering: {e}")
         raise
