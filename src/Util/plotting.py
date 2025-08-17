d9dd381d500199e210cc0609db0f3cf5d80b66a8
Davide <davide.mattioli.2001@gmail.com>
2025-08-17 18:55:55 +0200

    refactor plot_df and RPV modeling: streamline output directory handling, add skip logic for existing files, and include new plotting workflows with enhanced visualizations

--- src/Util/plotting.py
+++ src/Util/plotting.py
@@ -1,19 +1,45 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import polars as pl
+from pathlib import Path
 from Common.raster import plotting_raster
+import logging
 
 
-def plot_df(week, gdf, band):
+
+def plot_df(week, gdf, out_dir):
+
     for path in list(gdf["paths"]):
-        df = pl.read_parquet(path)
-        plotting_raster(df,"/run/media/mak/Heim/RPV_Results/V8",
-                        path.split("/")[-1].replace(".png","")+f"_{week}"
-                        ,debug=True,fill_empty=False,
-                        ny=300,
-                        nx=600,
-                        plot_density=True,
-                        dpi=500,
-                        auto_figsize = True, density_discrete=True
-)
+
+
+        try:
+            name = Path(path).stem
+            if (out_dir / "bands_data" / f"panels_{name}.png").exists():
+                logging.info(f"Skipping {name} as it already exists")
+                continue
+        except Exception as e:
+                continue
+
+        try:
+            df = pl.read_parquet(path)
+        except Exception as e:
+            print(e)
+            print(path)
+            continue
+
+        plotting_raster(
+            df,
+            out_dir,
+            name,
+            debug=True,
+            fill_empty=False,
+            ny=380,
+            nx=630,
+            plot_density=True,
+            dpi=500,
+            auto_figsize=True,
+            density_discrete=True,
+            density_mode="kde",
+        )
